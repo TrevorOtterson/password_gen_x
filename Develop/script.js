1ba7249334942f01a7f651390b0646c3e661b8fd
Trevor Otterson <totterson97@gmail.com>
2020-06-08 08:22:36 -0600

    Got my passwords to generate using button and generate to screen. Organized code better and properly.

--- Develop/script.js
+++ Develop/script.js
@@ -1,78 +1,60 @@
 // Variables
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lower = "abcdefghijklmnopqrstuvwxyz"
-var num = 1234567890
-var special = "!@#$%^&*()-+=_[]{};:?/><.,~`"
-var yourPass = ''
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var lower = 'abcdefghijklmnopqrstuvwxyz'
+var num = '1234567890'
+var special = '!@#$%^&*()-+=_[]{};:?/><.,~`'
 
 // Functions
 // Write password to the #password input
 function password(l, characters) {
   var pwd = '';
+  //l = int(l)
   for (var i = 0; i < l; i++) {
+    console.log(characters)
     pwd += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return pwd;
 }
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-
-// Random character generator
-// function generate(passLength) {
-//   for (var i = 0; i <= passLength; i++) {
-//     var passGen = Math.floor(Math.random() * passUpper.length + passLower.length + passSpecial.length + passNum.length);
-//     console.log(passGen);
-//   }
-// }
-
+function writePassword(){
+var characterSet = ''
 // Password Length if / else statement
 var passLength = prompt('How many characters do you want your password to be? *Can only be between 8 and 128 characters.*')
 if (passLength >= 8 && (passLength <= 128)) {
   console.log(passLength);
 }
 else {
-  alert("Password must be between 8 and 128 characters. Try again.");
+  alert('Password must be between 8 and 128 characters. Try again.');
 }
 
 // Upper case characters pass into password
-var passUpper = confirm("Would you like upper case characters in your password?");
+var passUpper = confirm('Would you like upper case characters in your password?');
 if (passUpper) {
-  password(upper)
+  characterSet += upper
 };
 
 // Lower case characters pass into password
-var passLower = confirm("Would you like lower case characters in your password?");
+var passLower = confirm('Would you like lower case characters in your password?');
 if (passLower) {
-  password(lower)
+  characterSet += lower
 };
 
 // Special characters pass into password
-var passSpecial = confirm("Would you like special characters in your password?");
+var passSpecial = confirm('Would you like special characters in your password?');
 if (passSpecial) {
-  password(special)
+  characterSet += special
 };
 
 // Numbers pass into password
-var passNum = confirm("Would you like numbers in your password?");
+var passNum = confirm('Would you like numbers in your password?');
 if (passNum) {
-  password(num)
+  characterSet += passNum
 };
 
+document.getElementById ('password').value = password (passLength, characterSet);
+}
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", function(p) {
-  (passUpper.confirm) ? characters += upper : '';
-  (passLower.confirm) ? characters += lower : '';
-  (passNum.confirm) ? characters += num : '';
-  (passSpecial.confirm) ? characters += special : '';
-
-  yourPass.value = password(passLength.value, characters);
-});
-
-debugger
+generateBtn.addEventListener('click', writePassword);
